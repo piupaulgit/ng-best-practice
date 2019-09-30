a6c2025c1db3a0779c6a29b77a709c1ad063bcf0
Piu Paul <31591817+piupaulgit@users.noreply.github.com>
2019-09-30 23:34:37 +0530

    update

--- frontend/src/app/common/services/common.service.ts
+++ frontend/src/app/common/services/common.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
+  BASE_PATH: string = 'http://localhost:3000/';
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  sidebarMenuList(): Observable<any>{
+    return this.http.get<any>(this.BASE_PATH + 'moduleName');
+  }
 }
