7de1e87290e2d1ec06f9368e2f18639bc5bd5040
Piu Paul <31591817+piupaulgit@users.noreply.github.com>
2019-09-30 23:32:19 +0530

    update

--- frontend/src/app/common/components/header/header.component.ts
+++ frontend/src/app/common/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TitleService } from '../../services/title.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  headerData;
+
+  constructor(private titleService: TitleService) { }
 
   ngOnInit() {
+    // init page title;
+    this.titleService.init();
+
+    // dynamic title and icon in header
+    this.titleService.headerData.subscribe(data => this.headerData = data); 
   }
 
 }
