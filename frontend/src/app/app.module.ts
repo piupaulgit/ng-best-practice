773652092d25b410fcc294c22bb4e47336256960
Piu Paul <31591817+piupaulgit@users.noreply.github.com>
2019-09-30 23:31:00 +0530

    update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -9,11 +10,12 @@ import { SidenavComponent } from './common/components/sidenav/sidenav.component'
 import { HeaderComponent } from './common/components/header/header.component';
 import { FiltersComponent } from './common/components/filters/filters.component';
 import { DataTablesComponent } from './common/components/data-tables/data-tables.component';
+import { TitleService } from './common/services/title.service';
 
 @NgModule({
   declarations: [AppComponent, SidenavComponent, HeaderComponent, FiltersComponent, DataTablesComponent],
-  imports: [BrowserModule, AppRoutingModule, DashboardModule, ProjectsModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, DashboardModule, ProjectsModule, HttpClientModule],
+  providers: [TitleService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
